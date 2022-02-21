f5dafafad950f0590a3d6414995e9d336c30cf0c
Josue Barros <joshbarrosweb@gmail.com>
2022-02-20 22:19:12 -0300

    chore: project setup successfully

---
+++ backend/src/main/java/com/joshbarrosweb/sdashboard/SdashboardApplication.java
@@ -0,0 +1,13 @@
+package com.joshbarrosweb.sdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdashboardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/joshbarrosweb/sdashboard/SdashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshbarrosweb.sdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
