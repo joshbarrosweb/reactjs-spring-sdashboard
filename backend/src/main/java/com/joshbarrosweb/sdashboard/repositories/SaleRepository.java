fee890b155a1bf1f422ffb7cc1b98bd4f27d0dc9
Josue Barros <joshbarrosweb@gmail.com>
2022-03-05 22:58:01 -0300

    chore: created grouped by amount and grouped by success search feature

--- backend/src/main/java/com/joshbarrosweb/sdashboard/repositories/SaleRepository.java
+++ backend/src/main/java/com/joshbarrosweb/sdashboard/repositories/SaleRepository.java
@@ -1,9 +1,21 @@
 package com.joshbarrosweb.sdashboard.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.joshbarrosweb.sdashboard.dto.SaleSuccessDTO;
+import com.joshbarrosweb.sdashboard.dto.SaleSumDTO;
 import com.joshbarrosweb.sdashboard.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 
+	@Query("SELECT new com.joshbarrosweb.sdashboard.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+				+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new com.joshbarrosweb.sdashboard.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 }
--- backend/src/main/java/com/joshbarrosweb/sdashboard/services/SaleService.java
+++ backend/src/main/java/com/joshbarrosweb/sdashboard/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.joshbarrosweb.sdashboard.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.joshbarrosweb.sdashboard.dto.SaleDTO;
+import com.joshbarrosweb.sdashboard.dto.SaleSuccessDTO;
+import com.joshbarrosweb.sdashboard.dto.SaleSumDTO;
 import com.joshbarrosweb.sdashboard.entities.Sale;
 import com.joshbarrosweb.sdashboard.repositories.SaleRepository;
 import com.joshbarrosweb.sdashboard.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
